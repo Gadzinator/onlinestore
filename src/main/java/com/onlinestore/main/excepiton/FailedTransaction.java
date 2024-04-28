673e2f32409ad1f6ef98afa2361c98e0ecf00b4a
grigorev <ilovemeet2022@gmail.com>
2024-04-28 16:04:38 +0300

    Deleted the main method and unnecessary exceptions

--- src/main/java/com/onlinestore/main/excepiton/FailedTransaction.java
+++
@@ -1,8 +0,0 @@
-package com.onlinestore.main.excepiton;
-
-public class FailedTransaction extends RuntimeException {
-
-	public FailedTransaction(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
